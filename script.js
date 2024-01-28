935581f4af894faa8e9e0ca044fbae8f28aa0aa6
alfajarjaya <sansanjaya40@gmail.com>
2024-01-28 19:32:56 +0700

    update

--- script.js
+++ script.js
@@ -6,14 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
     buttons.forEach(function(button) {
         button.addEventListener('click', function() {
             if (button.value === '=') {
-                try {
                     var result = new Function('return ' + currentExpression)();
                     display.value = result;
                     currentExpression = result.toString();
-                } catch (error) {
-                    display.value = 'Error';
-                    currentExpression = '';
-                }
             } else if (button.value === 'C') {
                 display.value = '';
                 currentExpression = '';
